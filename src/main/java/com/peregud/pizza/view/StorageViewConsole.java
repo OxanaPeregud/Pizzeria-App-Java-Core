3860232f0c4b28c1b47551ff7bc768b520ebdfe5
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-05-11 20:00:46 +0300

    add: jacoco coverage, mockito

--- src/main/java/com/peregud/pizza/view/StorageViewConsole.java
+++ src/main/java/com/peregud/pizza/view/StorageViewConsole.java
@@ -11,19 +11,19 @@ import com.peregud.pizza.repository.StorageRepository;
 import java.util.Map;
 
 public class StorageViewConsole implements StorageView {
-    private static final StorageRepository STORAGE = new StorageRepository();
+    private final StorageRepository storage = new StorageRepository();
 
     @Override
     public void displayStorage() {
         System.out.println("Ingredients in storage:");
-        for (Map.Entry<Ingredient, Integer> entry : STORAGE.entrySet()) {
+        for (Map.Entry<Ingredient, Integer> entry : storage.entrySet()) {
             System.out.println(entry.getKey() + " : " + entry.getValue());
         }
     }
 
     @Override
     public void showLackOfIngredients() {
-        for (Map.Entry<Ingredient, Integer> entry : STORAGE.entrySet()) {
+        for (Map.Entry<Ingredient, Integer> entry : storage.entrySet()) {
             Integer value = entry.getValue();
             if (value <= 10)
                 System.out.println("Ingredient " + entry.getKey() + " is running out, quantity is " + entry.getValue());
--- src/main/java/com/peregud/pizza/view/SupplyServiceViewConsole.java
+++ src/main/java/com/peregud/pizza/view/SupplyServiceViewConsole.java
@@ -9,7 +9,7 @@ import com.peregud.pizza.types.Supplier;
 import com.peregud.pizza.repository.StorageRepository;
 
 public class SupplyServiceViewConsole implements SupplyServiceView {
-    private static final StorageRepository STORAGE = new StorageRepository();
+    private final StorageRepository storage = new StorageRepository();
 
     @Override
     public void suppliersMenu() {
@@ -33,13 +33,13 @@ public class SupplyServiceViewConsole implements SupplyServiceView {
     public void supplierIngredientBefore(Supplier supplier) {
         System.out.println(supplier + " delivered " + supplier.getIngredient());
         System.out.println("Current quantity of " + supplier.getIngredient() + " in storage: "
-                + STORAGE.getIngredientQuantity(supplier.getIngredient()));
+                + storage.getIngredientQuantity(supplier.getIngredient()));
     }
 
     @Override
     public void supplierIngredientAfter(Supplier supplier) {
         System.out.println("Quantity of " + supplier.getIngredient() + " has been changed to: "
-                + STORAGE.getIngredientQuantity(supplier.getIngredient()));
+                + storage.getIngredientQuantity(supplier.getIngredient()));
     }
 
     @Override
